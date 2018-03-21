3892cb665a65afd63eaf56e651195f42a83f882f
Denis Vabishchevich <slad_den@mail.ru>
2018-03-21 23:43:06 +0300

    Initial commit

---
+++ app/src/main/java/android/bignerdranch/com/less_8_app_design/MainActivity.java
@@ -0,0 +1,20 @@
+package android.bignerdranch.com.less_8_app_design;
+
+import android.graphics.pdf.PdfRenderer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       // PdfRenderer pdf = new PdfRenderer(getSeac);
+
+    }
+
+
+
+}
